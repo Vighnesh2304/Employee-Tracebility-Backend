c31c26d9fe7538aa14326f0dfe30ea533bd87428
Vighnesh  Kandalgaonkar <78204375+Vighnesh2304@users.noreply.github.com>
2024-09-21 16:04:15 +0530

    Update dbconnection.js

--- utils/dbconnection.js
+++ utils/dbconnection.js
@@ -1,21 +1,23 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQL_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,  // You can adjust this limit as per your requirement
+  queueLimit: 0,
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Error connecting to MySQL:', err);
     return;
   }
   console.log(`MySql Database Connected! DB = ${process.env.MYSQL_DATABASE}`);
+  connection.release(); // Return the connection to the pool after use
 });
 
-
-
-module.exports = connection;
+module.exports = pool;
