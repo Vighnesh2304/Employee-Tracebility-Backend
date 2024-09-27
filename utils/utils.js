dca4361c49921fbd3fb87b52ae685c8c560aae4e
DwarkeshDeshmukh <105703508+DwarkeshDeshmukh@users.noreply.github.com>
2024-09-28 01:19:17 +0530

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,10 +1,7 @@
 //cors option
 const corsOptions = {
-  origin: ['https://employee-tracebility.vercel.app'], // allow only requests from this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: 'https://employee-tracebility.vercel.app',
   credentials: true,
-  preflightContinue: true, // add this option to handle preflight requests
 };
 
 module.exports = { corsOptions };
