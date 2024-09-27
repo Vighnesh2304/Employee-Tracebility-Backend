be8f25ad2cc6e031d6d330d0f16d0046120161d9
Priyanshu Lapkale <66089621+mh31-priyanshu@users.noreply.github.com>
2024-09-27 12:08:08 +0530

    CORS policy update

--- utils/utils.js
+++ utils/utils.js
@@ -1,14 +1,8 @@
 //cors option
 const corsOptions = {
-    origin: [
-        "http://localhost:5173",
-        "http://localhost:4173",
-        "http://localhost:3001",
-        process.env.CLIENT_URL,
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  };
+  origin: true, // Allow all origins
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
 
-
-module.exports = {corsOptions}
+module.exports = { corsOptions };
