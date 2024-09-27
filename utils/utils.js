4b31b9fd3db92bac4be611ba1b11c50b3733828e
DwarkeshDeshmukh <105703508+DwarkeshDeshmukh@users.noreply.github.com>
2024-09-28 00:58:03 +0530

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,6 @@
 //cors option
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5174',
   methods: '*',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
