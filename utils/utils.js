7357d60df1ef8f2e372626463be8381bd3cae74e
DwarkeshDeshmukh <105703508+DwarkeshDeshmukh@users.noreply.github.com>
2024-09-28 01:02:16 +0530

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,9 +1,10 @@
 //cors option
 const corsOptions = {
-  origin: '*',
-  methods: '*',
+  origin: ['http://localhost:5174'], // allow only requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
+  preflightContinue: true, // add this option to handle preflight requests
 };
 
 module.exports = { corsOptions };
